4f53ee85cc4725a01c79b043e9cf7083dfde74a5
mlawan <mbklawan@gmail.com>
2020-02-10 00:23:38 +0300

    exercise 3 maths

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,1 +1,3 @@
 print("hello Boss")
+# Boss im doing my commments as next chapter two
+print("Sir, it's working as expected")
---
+++ ex3/ex3.py
@@ -0,0 +1,9 @@
+#maths and Calculation
+print("I could like to count my chicken")
+print("Hens", 20 + 30 / 6)
+print("Rossters", 100 - 20 * 3 / 2)
+print("I could like to count egss:")
+print(2 + 45 - 23 / 3 * 10 % 5)
+print("It's true that 3 + 2 -4 < 5 -7?")
+print(3+2<5-7)
+print("Thats false")
