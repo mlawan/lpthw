4ab99298067f1ad0bdbce9010bec424172ccf005
mlawan <mbklawan@gmail.com>
2020-02-10 00:59:39 +0300

    new exercise about stings parenthesis and calculations

---
+++ ex4/ex4.py
@@ -0,0 +1,16 @@
+#another program to run
+my_name = "Alhamdu Bello"
+my_age = 35 # its almost
+my_height = 74
+my_weight = 400
+my_eyes = 'white' #brownish
+my_teeth = 'cream'
+my_hair = 'black'
+
+print(f"lets talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.!")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"He's {my_teeth} teeth are beautiful")
+total = my_age + my_height + my_weight
+print(f"if i add {my_age}, {my_weight}, and {my_height} i will get {total}.")
